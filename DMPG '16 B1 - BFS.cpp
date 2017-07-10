cde1ef4085bb34b615bf746355b4b44ece6bc5fc
Mark Chen <markchen8717@gmail.com>
2017-07-10 19:05:40 -0400

    Update DMPG '16 B1 - BFS.cpp

--- DMPG '16 B1 - BFS.cpp
+++ DMPG '16 B1 - BFS.cpp
@@ -1,6 +1,3 @@
-
-
-#include "stdafx.h"
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,36 +5,9 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
-	int T, N, B;
-	double output = 0;
-	int S [99999] ;
-	cin >> T;
-	for (int i = 0; i < T; i++)
-	{
-		cin >> N >> B;
-		for (int k = 0; k < N-1; k++)
-		{
-			cin >> S[k];
-		}
-		S[N-1] = B;
-		sort(S, S + N);
-		int index;
-		for (int j = 0; j < N; j++)
-		{
-			if (S[j] > B)
-			{
-				index = j;
-				break;
-			}
-		}
-		
-		output = sqrt(N - (N - index )) * 100;
-		cout << "Bob wins $" << setprecision(2) << fixed << output << " at IOI!"<<"\n";
-	}
+	int n, d, q, l, t;
+	cin >> n >> d>> q>>l>> t;
+	cout << (n*5 + d* 10 + q*25 + l * 100 + t * 200);
 
-	
-
-
+	return 0;
 }
-
