c2e085407229f0eaa49a03bd4d4c8aae66ac6507
Mark Chen <markchen8717@gmail.com>
2017-07-10 19:11:38 -0400

    Update DMPG '17 B2 - Yoda.cpp

--- DMPG '17 B2 - Yoda.cpp
+++ DMPG '17 B2 - Yoda.cpp
@@ -1,9 +1,7 @@
-// DMPG '17 B2 - Yoda.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include <string>
 #include <iostream>	
+#include <stdio.h>
+#include <ctype.h>
 using namespace std;
 
 
@@ -11,32 +9,46 @@ int main()
 {
 	string input;
 	getline(cin, input);
-	
-	int comma = input.find(',');
 	string str1;
 	string str2;
-	if (!isalpha(input[comma + 2]))
-	{
-		cout << input.substr(0, input.length() - 1) + input.substr(input.length() - 1);
-	}
-	str1 = input.substr(comma+2,input.size()-3-comma);
-	str2 = input.substr(0, comma);
-
-	
-
 	string output;
-	
-	
-	str1[0] = str1[0] - 32;
-	str2[0] = str2[0] + 32;
+	int comma = input.find(',');
+	if (input.length() - 1 - comma > 1)
+	{
+		
+		str1 = input.substr(comma+2, input.length() - 3 - comma);
 
-	output = str1 + " " + str2 + input.substr(input.size() - 1);
+		if (isalpha(str1[0]))
+		{ 
+			str1[0] = toupper(str1[0]);
+		}
+		
+		
+		str2 = input.substr(0, comma);
 
+		if (isalpha(str2[0]))
+		{
+			str2[0] = tolower(str2[0]);
+		}
+
+		output = str1; 
+		if (input.length() - comma < input.length())
+		{
+			
+
+			output += + " " +str2;
+		}
+		
+		output += input.substr(input.length() - 1);
+
+	}
+	else
+	{
+		output = input.substr(0, input.length() - 2) +
+			input.substr(input.length() - 1);
+	}
 
 	cout << output;
-	
-
 
 
 }
-
