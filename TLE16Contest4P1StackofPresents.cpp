a47ec3076290bc1f8477d416bf73ef450f14bf6b
Mark Chen <markchen8717@gmail.com>
2017-09-27 17:27:49 -0400

    Add files via upload

---
+++ TLE16Contest4P1StackofPresents.cpp
@@ -0,0 +1,36 @@
+// TLE16Contest4P1StackofPresents.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int input, sum, cnt;
+int weights[100000];
+
+int main()
+{
+
+	cin >> input;
+
+	for (int i = 0; i < input; i++)
+	{
+		cin >> weights[i];
+	}
+
+	sort(weights, weights + input);
+
+
+	for (int i = 0; i < input; i++)
+	{
+		if (weights[i] >= sum) {
+			sum += weights[i];
+			cnt++;
+		}
+	}
+
+	cout << cnt;
+
+}
