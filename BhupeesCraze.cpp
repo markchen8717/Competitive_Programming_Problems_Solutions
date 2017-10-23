2597330417b7f215c860cb2c2520fcabeadb4d37
Mark Chen <markchen8717@gmail.com>
2017-10-23 13:15:49 -0400

    Update BhupeesCraze.cpp

--- BhupeesCraze.cpp
+++ BhupeesCraze.cpp
@@ -1,5 +1,41 @@
-//  Bhupee's Craze 
-//Basic Implementation
+/*
+Bhupee's Craze https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/bhupees-craze-7d960eab-24ff51e3/
+
+Craze of programming always have been there in industries, so maximum number of companies are trying to get in touch with good programmers. For this a well established company "SundayCoders" is organizing a party for interested programmers.Bhupee is interested for this function too. But here comes a problem, the party is open for couples only. So he asked his friends to help him out. They suggested him some names,but due to Bhupee's obsession towards factorials, he will select the girl whose name has minimum factorial value of the occurence of unique characters. Can you help him out?(Move to sample i/o for further details):
+
+Input Format:
+
+First line of input will contain an integer T, number of TestCases. First line of each testcase will contain an integer N(number of names). Each of Next N lines will contain a string consisting of lowercase alphabets only.
+
+Output Format:
+
+Print T lines, each line having string (Minimum factorial value of the occurence of unique characters in names). If there is no name the output will be previous string.
+
+Constraints:
+
+1 <= T <= 100 0 <= N <= 25 1 <= length(string) <= 50
+
+Note:- If more than one name have same factorial value, print the last one.
+
+Sample Input:
+
+2
+3
+sarahah
+akanksha
+aparana
+5
+saraswati
+shyamapadmavatialkasundarishushmavati
+shivgami
+devsena
+deeksha
+Sample Output:
+
+aparana
+deeksha
+*/
+
 
 #include "stdafx.h"
 #include <bits/stdc++.h>
