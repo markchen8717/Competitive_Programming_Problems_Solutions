ee13a18c94d5d743a785b03689814525f7ef514c
Mark Chen <markchen8717@gmail.com>
2017-10-23 13:26:10 -0400

    Update DMPG'16 B3 - CCO.cpp

--- DMPG'16 B3 - CCO.cpp
+++ DMPG'16 B3 - CCO.cpp
@@ -1,5 +1,45 @@
-// DMPG'16 B3 - CCO.cpp : Defines the entry point for the console application.
-//
+/*
+
+DMPG '16 B3 - CCO
+
+https://dmoj.ca/problem/dmpg16b3
+
+Bob is now so rich that his ambitions for his purchases have begun to spiral out of control. Everything that he once thought would be able to satisfy his needs no longer comes close. The only way to quench such lofty desires is by making money!
+
+Bob is an expert in information security and is able to effectively cover the tracks of his underground affairs. In order to sustain such a herculean operation, he has hired you to help! Help Bob by printing out the design of the Canadian Counterfeiting Operation illegal tender.
+
+Input Specification
+
+Each test case will consist of 33 lines each containing 33 characters, representing the 33 by 33 design to be printed on the bill.
+
+The line afterwards consists of 33 space separated integers VV, HH and WW, representing the value of the bank note (100≤V≤1000)(100≤V≤1000) and the number of line breaks above and below the central design, and the number of spaces to the left and right of the central design (1≤H,W≤50)(1≤H,W≤50) respectively.
+
+Output Specification
+
+Your program should output a rectangular array of characters consisting of = characters (ASCII 6161) along the top and | characters (ASCII 124124) down the sides.
+
+The value of the counterfeit bill should appear justified in the top-left and bottom-right corners of the tender.
+
+The 33 by 33 design given in the input should be centered in the bill, with HH lines of space characters (ASCII 3232) above and below it, and VV space characters bordering it.
+
+Sample Input
+
+Copy
+[|]
+/ \
+\_/
+100 1 13
+Sample Output
+
+Copy
+===============================
+|100                          |
+|             [|]             |
+|             / \             |
+|             \_/             |
+|                          100|
+===============================
+*/
 
 
 #include <bits/stdc++.h>
