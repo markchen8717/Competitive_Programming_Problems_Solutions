ae8a70da193e647d99e9d52d0ef9b70794bff460
Mark Chen <markchen8717@gmail.com>
2017-10-23 13:28:54 -0400

    Update TLE '17 Contest 1 P1 - String Rounding.cpp

--- TLE '17 Contest 1 P1 - String Rounding.cpp
+++ TLE '17 Contest 1 P1 - String Rounding.cpp
@@ -1,5 +1,50 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
-//
+/*
+TLE '17 Contest 1 P1 - String Rounding
+
+https://dmoj.ca/problem/tle17c1p1
+
+Fax McClad, Croneria's most adventurous bounty hunter, has been tasked to explore the unknown planet TR-387. Upon arrival to this planet, he finds a primitive civilization of alien beings. He is surprised to learn that they only use lowercase English letters to represent digits in their number system!
+
+The beings of this planet are particularly fond with a technique called rounding. Here are the rules on how to round a number:
+
+Start with the last (rightmost) digit of the number
+If the digit is in between a and m, set the digit to a.
+If the digit is in between n and z, set the digit to a and increase the next (left) digit by one character. If the next digit happens to be z, round that character instead of increasing it. If there is no next digit, add a to the left of the number.
+Fax wishes to be acquainted with this alien species by showing them that he knows their rounding technique, but he is having difficulty doing so. Given a number SS, can you round it for him?
+
+Input Specification
+
+The first and only line of input will contain SS, a string consisting only of lowercase English letters. SS will contain at least 11 character and at most 100100 characters.
+
+Output Specification
+
+Output one line, the rounded value of SS.
+
+Sample Input 1
+
+Copy
+abcdefghijklmnopqrstuvwxyz
+Sample Output 1
+
+Copy
+abcdefghijklmnopqrstuvwxza
+Sample Input 2
+
+Copy
+y
+Sample Output 2
+
+Copy
+aa
+Sample Input 3
+
+Copy
+faxmcladisthegreatestbountyhunterincroneria
+Sample Output 3
+
+Copy
+faxmcladisthegreatestbountyhunterincroneria
+*/
 
 #include "stdafx.h"
 #include <bits/stdc++.h>
