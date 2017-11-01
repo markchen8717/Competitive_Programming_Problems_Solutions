b55d50f097604203a66f206d8db864a2bb82b9a6
Mark Chen <markchen8717@gmail.com>
2017-11-01 13:14:20 -0400

    Update Lampost.cpp

--- Lampost.cpp
+++ Lampost.cpp
@@ -1,5 +1,45 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
-//
+/*
+
+Lamppost
+
+https://dmoj.ca/problem/hci16lamppost
+
+Task Description
+
+You are an electrician working in Moonlight City. One night, the city council gave you a call and informed you that the lampposts in Moonlight Square are all out of order, so they need your help. Moonlight Square is divided into NN zones, each of which contains a single lamppost. When you arrived in Moonlight Square, you realised that you only have one light bulb in your toolbox, and so you can only repair one lamp. The lampposts are numbered 11 to NN and each lamppost can light up a certain number of zones around it. Decide which lamppost to repair so that the most number of zones will be lit.
+
+Input
+
+The first line consists of two integers, NN and CC. The next CC lines contain two integers each, C1C1 and C2C2, indicating that C1C1 can be lit from C2C2 and vice versa.
+
+Output
+
+Output the index of the lamppost which, when repaired, will light up the largest number of zones in the Square. Ties are broken by choosing the lamppost with the numerically largest index.
+
+Limits
+
+For all test cases, 1≤N≤1000000,1≤C≤10000001≤N≤1000000,1≤C≤1000000
+
+Subtask 1 (20%): 1≤N<1000,1≤C<10001≤N<1000,1≤C<1000
+
+Subtask 2 (80%): 1000≤N≤1000000,1000≤C≤10000001000≤N≤1000000,1000≤C≤1000000
+
+Sample Input
+
+Copy
+10 5
+1 2
+1 3
+1 4
+1 5
+2 6
+Sample Output
+
+Copy
+1
+
+
+*/
 
 #include "stdafx.h"
 #include <bits/stdc++.h>
